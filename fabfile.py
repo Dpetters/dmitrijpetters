a1c65fde5f04aad56df5cd8e3832f30884939010
Dmitrij Petters <dpetters@mit.edu>
2011-10-20 17:38:40 -0400

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+from fabric.api import env, sudo, cd, run
+
+STAGING_PASSWORD = "Ang3lInvest1ng"
+
+__all__= ["restart", "update"]
+
+env.password = STAGING_PASSWORD
+env.directory = 'var/www/dmitrijpetters.com'
+env.hosts = ['root@staging.umeqo.com']
+
+def restart():
+    run('service apache2 restart')
+
+def update():
+    with cd(env.directory):
+        run("git pull")
+        restart()
